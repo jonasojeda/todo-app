6a420b4d2b23eec15440d15772579f61f497b2ec
jonasojeda <76564885+jonasojeda@users.noreply.github.com>
2023-01-18 16:57:34 -0300

    implementation of handleInputChange function

--- src/components/TodoForm/TodoForm.jsx
+++ src/components/TodoForm/TodoForm.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const initialFormValues={
+  title: '',
+  description:''
+}
 
 const TodoForm = () => {
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const {title, description}= formValues
+
+  const handleInputChange = (e)=>{
+
+    const changedFormValue = {
+      ...formValues,
+      [e.target.name]:e.target.value
+    }
+
+    setFormValues(changedFormValue)
+  }
+
+  
   return (
     <div>
-        <h1>TodoForm</h1>
+        <h1>Nueva Tarea</h1>
+        <form>
+          <input 
+            type="text" 
+            placeholder='Titulo' 
+            className='form-control' 
+            value={title}
+            name='title'
+            onChange={handleInputChange}
+          />
+          <textarea 
+            placeholder='Description' 
+            className='form-control mt-2'
+            value={description}
+            name='description'
+            onChange={handleInputChange}
+          />
+          <button 
+            className='btn btn-primary btn-block mt-2'
+          >Agregar tarea
+          </button>
+        </form>
     </div>
   )
 }
